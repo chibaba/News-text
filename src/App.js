646302e16c04ceba08fcd2c774777ebb70d900ec
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-20 13:18:37 +0100

    fixing routes for users

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import './App.css';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+
+import Authors from './authors/pages/Authors'
+
 
 const App = () => {
-  return <di>hello world</di>
+  return (
+       <Router>
+  <Route path="/">
+    <Authors/>  
+  </Route>
+
+  </Router>
+  )
 }
 
 export default App;
