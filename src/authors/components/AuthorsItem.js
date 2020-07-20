6f07d58ee05b1284909c1c278f1ebb977551dd10
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-20 23:51:26 +0100

    styling Ui COmponents

--- src/authors/components/AuthorsItem.js
+++ src/authors/components/AuthorsItem.js
@@ -1,5 +1,7 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
+import  Avatar from '../../common/components/UIElements/Avatar'
 import './AuthorsItem.css';
 
 
@@ -8,19 +10,18 @@ const AuthorsItem = props => {
         <li className="authors-list">
           <div className="authors-item__content">
           <div className="authors-item__image">
-              <img src={props.image} alt={props.name} />
+              <Avatar image={props.image} alt={props.name} />
           </div>
           <div className="authors-item__info">
            <h2>{props.title}</h2>
-           <h2>{props.subject}</h2>
            <h2>{props.category}</h2>
+           <h2>{props.subject}</h2>
+          <Link to={`/${props.id}/News`}>
            <h3>{props.body}</h3>
-           <h3>{props.date}</h3>
+          </Link>
            <h3>{props.NoStories} {props.NoStories === 1 ? 'story' : 'More News'}</h3>
-           
-
+           <h3>{props.date}</h3>
           </div>
-
           </div>
 
         </li>
