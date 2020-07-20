30236530f47ef92a72f8c527d8c8c26d64d0071f
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-20 14:04:18 +0100

    switch router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
-import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Redirect, Switch} from 'react-router-dom';
 
-import Authors from './authors/pages/Authors'
+import Authors from './authors/pages/Authors';
+import NewNews  from './news/pages/NewNews'
 
 
 const App = () => {
   return (
        <Router>
-  <Route path="/">
+       <Switch>
+  <Route path="/" exact>
     <Authors/>  
   </Route>
-
+  <Route path="/News/New" exact>
+    <NewNews/>  
+  </Route>
+  <Redirect to="/" />
+    </Switch>
   </Router>
   )
 }
