637c5857972053c7551b8f1e5c7bee2532db1403
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-20 18:53:03 +0100

    created list of authors

---
+++ src/authors/components/AuthorsItem.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import '/AuthorsItem.css'
+
+
+const AuthorsItem = props => {};
+
+export default AuthorsItem;
---
+++ src/authors/components/AuthorsList.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+import '/AuthorsList.css'
+
+
+const AuthorsList = props => {};
+
+export default AuthorsList;
