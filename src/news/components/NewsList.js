28c00cce70f032cc68765e3864edde69937c1906
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-21 19:39:28 +0100

    using dynamic route

--- src/news/components/NewsList.js
+++ src/news/components/NewsList.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import Card from '../../common/components/UIElements/Card'
 import './AuthorsList';
+import NewsItem from './NewsItem';
 
 const AuthorsList = props => {
     if (props.items.length === 0) {
@@ -16,7 +17,16 @@ const AuthorsList = props => {
         );
     }
     return <ul className="news-list">
-        
+     {props.items.map(news => <NewsItem key={news.id} 
+     id={news.id} 
+     title={news.title} 
+     subject={news.subject} 
+     category={news.category}
+     body={news.body}
+     image={news.image}
+     date={news.date}
+      />)}
+
     </ul>
 }
 
