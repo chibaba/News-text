a7eb9f4e57caa31cdf2d6a680abb2e15649ef866
chibaba onyeoka <chinedu.onyeoka@gmail.com>
2020-07-21 01:50:26 +0100

    working on a UI cOMPONENT

--- src/authors/pages/Authors.js
+++ src/authors/pages/Authors.js
@@ -7,8 +7,8 @@ const Authors = () => {
         id: 'u1',
         title: 'Performace Complexity',
         category: 'Tech News',
-        subject: 'Every booster still undergoes detailed inspections between launches.',
-        body: "82 with 42 posters participating\r\n<ul><li>\r\n Falcon 9 B1058.2 stands tall on SLC-40, ready to launch the South Korean militarys Anasis 2 communications satellite. \r\n</li><li>\r\n This booster 1058.2 is… [+2638 chars]",
+        subject: 'Every booster still .',
+        body: "82 with 42 posters participating\r\n<ul><li>\r\n Falcon 9 B1058.2 stands tall on SLC-40,",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcRc96kcLicYy25CFi7P_ocMargwSC_vjRxIMg&usqp=CAU",
         date:"2020-07-20T16:18:08Z",
         NoStories: 3
